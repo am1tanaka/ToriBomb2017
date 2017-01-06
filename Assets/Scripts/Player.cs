bc7193616afb3edceb581afd9da4135436bc5e28
YuTanaka <YRK00337@nifty.com>
2017-01-06 19:31:35 +0900

    クリックした場所に爆弾を設定

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	/** 爆弾プレハブ*/
+	public GameObject prefBomb;
+
+	// Update is called once per frame
+	void Update () {
+		if (GameController.me.nowScene != GameController.SCENES.SC_GAME) {
+			return ;
+		}
+		// クリックを判定
+		if (Input.GetMouseButtonDown(0)) {
+			// クリック場所をチェック
+			Vector3 mspos = Input.mousePosition;
+			mspos.z = -Camera.main.transform.position.z;
+			Vector3 pos = Camera.main.ScreenToWorldPoint(mspos);
+			Instantiate(prefBomb, pos, Quaternion.identity);
+		}
+	}
+}
