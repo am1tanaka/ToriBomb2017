dbd791935420f78d410e0f71d7f52ebbc6713180
YuTanaka <YRK00337@nifty.com>
2017-01-06 17:20:58 +0900

    卵の出現

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,20 @@ public class GameController : MonoBehaviour {
 	/** デバッグ*/
 	public bool isDebug = true;
 
+	/** 卵の出現範囲。xは幅。yは高さ*/
+	public Vector3 eggRange = new Vector3(5,11,0);
+	/** 最初の卵が出現するまでの秒数*/
+	public float FIRST_OUT = 3f;
+	/** 最初の出現率。何秒に1つ出現するか*/
+	public float INIT_OUT_RATE = 3f; 
+	/** 最高レベル*/
+	public float MAX_OUT_RATE = 1f;
+	/** 最高レベルに達するまでの秒数*/
+	public float MAX_LEVEL_TIME = 60f;
+
+	/** 経過秒数*/
+	public float gameTime;
+
 	/** シーンのトップオブジェクト*/
 	public GameObject []sceneObjects;
 
@@ -16,6 +30,9 @@ public class GameController : MonoBehaviour {
 	public Text textScene;
 	/** スコアテキスト*/
 	public Text textScore;
+
+	/** 卵プレハブ*/
+	public GameObject prefEgg;
 
 	/** スコアの上限*/
 	private const int SCORE_MAX = 9999999;
@@ -76,7 +93,9 @@ public class GameController : MonoBehaviour {
 			case SCENES.SC_GAME:
 				textScene.text = "GAME";
 				sceneObjects[(int)SCENES.SC_TITLE].SetActive(false);
+				// ゲームパラメーターを初期化
 				iScore = 0;
+				gameTime = 0f;
 				break;
 			case SCENES.SC_GAMEOVER:
 				textScene.text = "GAMEOVER";
@@ -92,25 +111,67 @@ public class GameController : MonoBehaviour {
 		string sc = "00000"+iScore;
 		textScore.text = "Score:"+sc.Substring(sc.Length-6,6);
 
-		/** 切り替えチェック*/
-		if (isDebug) {
-			switch (nowScene) {
-			case SCENES.SC_TITLE:
-				if (Input.GetMouseButtonDown(0)) {
-					nextScene = SCENES.SC_GAME;
-				}
-				break;
-			case SCENES.SC_GAME:
-				if (Input.GetMouseButtonDown(1)) {
+		/** シーン別処理*/
+		switch (nowScene) {
+		case SCENES.SC_TITLE:
+		// タイトルの切り替え
+			if (Input.GetMouseButtonDown(0)) {
+				nextScene = SCENES.SC_GAME;
+			}
+			break;
+		case SCENES.SC_GAME:
+			updateGame();
+			/** 切り替えチェック*/
+			if (isDebug) {
+								if (Input.GetMouseButtonDown(1)) {
 					nextScene = SCENES.SC_GAMEOVER;
 				}
-				break;
-			case SCENES.SC_GAMEOVER:
-				if (Input.GetMouseButtonDown(0)) {
-					nextScene = SCENES.SC_TITLE;
-				}
-				break;
 			}
+
+			break;
+		case SCENES.SC_GAMEOVER:
+			// シーンの切り替え
+			if (Input.GetMouseButtonDown(0)) {
+				nextScene = SCENES.SC_TITLE;
+			}
+			break;
 		}
+	}
+
+	/** ゲームの更新処理*/
+	void updateGame() {
+		spawnEgg();
+	}
+
+	/** 卵を出現させる*/
+	void spawnEgg() {
+		// 出現率の向上
+		gameTime += Time.deltaTime;
+		float span = Mathf.Clamp(gameTime, 0f, MAX_LEVEL_TIME);
+		float nowRate = Mathf.Lerp(INIT_OUT_RATE, MAX_OUT_RATE, span/MAX_LEVEL_TIME);
+
+		// 1フレームの秒数: Time.deltaTime
+		// 1秒あたりのフレーム数: 1f/Time.deltaTime
+		float frame_sec = 1f/Time.deltaTime;
+		// 出現率: 1/(nowRate*1秒あたりのフレーム数)
+		float frame_num = nowRate*frame_sec;
+		// 乱数
+		float rand = Random.value*frame_num;
+		// 上記が1以下の時、卵を出現
+		if (rand <= 1f) {
+			Vector3 pos = new Vector3(Random.Range(-eggRange.x, eggRange.x), eggRange.y, 0f);
+			Instantiate(prefEgg, pos, Quaternion.identity);
+		}
+	}
+
+	/** ギズモの描画*/
+	void OnDrawGizmos() {
+		// 卵の出現範囲
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(
+			new Vector3(-eggRange.x, eggRange.y, eggRange.z),
+			new Vector3(eggRange.x, eggRange.y, eggRange.z)
+		);
+
 	}
 }
