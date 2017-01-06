1b149e808058908ea59527d12c51c4d98f1ac45e
YuTanaka <YRK00337@nifty.com>
2017-01-06 19:22:49 +0900

    爆発の当たり判定が抜けていたので追加

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+
+	/** タグを変更する*/
+	void changeExplosionTag() {
+		tag = "Exp";
+	}
+
+	/** 自分を消す*/
+	void destroyMe() {
+		Destroy(gameObject);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
